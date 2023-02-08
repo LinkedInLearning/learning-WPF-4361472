ff269cdcacd45ae34b9d7ea1408794e52cfc6db1
DESKTOP-S959DPI\macni <mac.nishimura@gmail.com>
2023-02-08 11:42:37 +0900

    チャプター05のサンプルコードを追加

---
+++ 05_07/src/SampleDeleteEventWpf/SampleDeleteEventWpf/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleDeleteEventWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            button.Click += Button_Click;
+            button.Click -= Button_Click;
+
+            Sample sampleA = new Sample("A");
+            button.Click += sampleA.SampleEvent;
+
+            Sample sampleB = new Sample("B");
+            button.Click += sampleB.SampleEvent;
+
+            button.Click -= sampleA.SampleEvent;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Button_Click");
+        }
+    }
+
+    public class Sample
+    {
+        private string name = string.Empty;
+
+        public Sample(string name)
+        {
+            this.name = name;
+        }
+        public void SampleEvent(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine($"SampleEvent {name}");
+        }
+    }
+}
