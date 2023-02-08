8de6bcc7dd672a8879624625aeebfc1c14849667
DESKTOP-S959DPI\macni <mac.nishimura@gmail.com>
2023-02-08 11:36:58 +0900

    チャプター03のサンプルコードを追加

---
+++ 03_02/src/SampleLayoutControll/SampleLayoutControll/CanvasSample.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleLayoutControl
+{
+    /// <summary>
+    /// CanvasSample.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CanvasSample : Page
+    {
+        public CanvasSample()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page stackPanel = new StackPanelSample();
+            NavigationService.Navigate(stackPanel);
+        }
+    }
+}
---
+++ 03_04/src/SampleDataControll/SampleDataControll/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SampleDataControll
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dataGrid.ItemsSource = new List<Person>()
+            {
+                new Person() {Id = 1, Name = "ABC", Age = 25 },
+                new Person() {Id = 2, Name = "DEF", Age = 22 },
+                new Person() {Id = 3, Name = "GHI", Age = 24 }
+            };
+
+            listBox.ItemsSource = new List<string>()
+            {
+                "ABC",
+                "DEF",
+                "GHI"
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
